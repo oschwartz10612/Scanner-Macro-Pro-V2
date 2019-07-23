7264cd177923a1f33d3b4052170f782c63038fe4
Owen Schwartz <oschwartz10612@gmail.com>
2019-07-23 19:41:05 -0400

    header removed for parser

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,59 @@
 #include <SPI.h>
 #include <Keyboard.h>
 
-#include <Parser.h>
-#include <Scripts.h>
+#include <Scripts.h> //User scripts
 
 String msg;
+
+class ScanParser : public HIDReportParser {
+  public:
+    ScanParser();
+    void Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
+  protected:
+    uint8_t KeyToAscii(bool upper, uint8_t mod, uint8_t key);
+    virtual void OnKeyScanned(bool upper, uint8_t mod, uint8_t key);
+    virtual void OnScanFinished();
+};
+
+ScanParser::ScanParser() {}
+
+void ScanParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
+  // If error or empty, return
+  if (buf[2] == 1 || buf[2] == 0) return;
+
+  for (uint8_t i = 7; i >= 2; i--) {
+    // If empty, skip
+    if (buf[i] == 0) continue;
+
+    // If enter signal emitted, scan finished
+    if (buf[i] == UHS_HID_BOOT_KEY_ENTER) {
+      OnScanFinished();
+    }
+
+    // If not, continue normally
+    else {
+      // If bit position not in 2, it's uppercase words
+      OnKeyScanned(i > 2, buf, buf[i]);
+    }
+
+    return;
+  }
+}
+
+uint8_t ScanParser::KeyToAscii(bool upper, uint8_t mod, uint8_t key) {
+  // Letters
+  if (VALUE_WITHIN(key, 0x04, 0x1d)) {
+    if (upper) return (key - 4 + 'A');
+    else return (key - 4 + 'a');
+  }
+
+  // Numbers
+  else if (VALUE_WITHIN(key, 0x1e, 0x27)) {
+    return ((key == UHS_HID_BOOT_KEY_ZERO) ? '0' : key - 0x1e + '1');
+  }
+
+  return 0;
+}
 
 void ScanParser::OnKeyScanned(bool upper, uint8_t mod, uint8_t key) {
   uint8_t ascii = KeyToAscii(upper, mod, key);
