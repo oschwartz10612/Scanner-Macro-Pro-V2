23503328dd8d65ab35cf184916331df4f5946d62
Schwartz <1439726@fcps.edu>
2019-07-30 13:36:40 -0400

    fixed error in h file

--- src/scripts/pxiID.h
+++ src/scripts/pxiID.h
@@ -1,34 +1,42 @@
 #include <includes.h>
 
-//Just scan barcode and finish with "FCPSON" in PXI
+//Snippit for barcodes with student ids in PXI
 bool script(String msg) {
     int delayTime = 50;
     if (msg != "") {
-        Keyboard.write(9); //TAB
-        delay(delayTime);
-        Keyboard.print("2390N");
-        delay(delayTime);
-        Keyboard.print(msg);
-        delay(delayTime);
-        Keyboard.write(9); //TAB
-        delay(delayTime);
-        Keyboard.print("FCPSON");
-        delay(delayTime);
-        Keyboard.write(9); //TAB
-        delay(delayTime);
-        Keyboard.write(9); //TAB
-        delay(delayTime);
-        Keyboard.write(9); //TAB
-        delay(delayTime);
-        Keyboard.write(111); //o
-        delay(delayTime);
-        Keyboard.write(176); //Enter
-        delay(delayTime);
-        Keyboard.write(176); //Enter
-  }
-  return true;
+        //Check if it is a barcode by looking at the first two numbers
+        bool barcode = false;
+        for (int i = 0; i < 2; i++) {
+            if (String(msg[i]) == "0") {
+                barcode = true;
+            } else {
+                barcode = false;
+            }
+        }
+
+        if(barcode) {
+            Keyboard.write(9); //TAB
+            delay(delayTime);
+            Keyboard.print("2390N");
+            delay(delayTime);
+            Keyboard.print(msg);
+            delay(delayTime);
+            Keyboard.write(9); //TAB
+        } else {
+            Keyboard.print(msg);
+            delay(delayTime);
+            Keyboard.write(9); //TAB
+            delay(delayTime);
+            Keyboard.write(9); //TAB
+            delay(delayTime);
+            Keyboard.write(9); //TAB
+            delay(delayTime);
+            Keyboard.write(111); //o
+            delay(delayTime);
+            Keyboard.write(176); //Enter
+            delay(delayTime);
+            Keyboard.write(176); //Enter
+        }
+    }
+    return true;
 }
-
-
-
-
